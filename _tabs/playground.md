de7ef2a0d3e816dd1f2426c12e2d319f088908d6
YohiroW <yohiro36@foxmail.com>
2023-07-04 21:13:36 +0800

    Update playground

--- _tabs/playground.md
+++ _tabs/playground.md
@@ -4,10 +4,12 @@ icon: fa-solid fa-gamepad
 order: 4
 ---
 
-<div class='threejs'>
-    <div id='cube'></div>
+<div id="cube" style="--aspect-ratio:16/9;">
+  <div id="param" class="dat-gui">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dat-gui/0.7.7/dat.gui.min.js"></script>
+    <script src='/assets/js/playground.js'></script>
+  </div>
 </div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
-<link rel="stylesheet" href="/assets/style.scss">
-<script src='/assets/js/playground.js'></script>
+
--- assets/js/playground.js
+++ assets/js/playground.js
@@ -1,11 +1,14 @@
 var camera, scene, renderer;
 var geometry, material, mesh;
+var container;
+var t_prev = Date.now() * 0.001;
+var params = { rpm_x: 7.0, rpm_y: -5.0, rpm_z: 0.0 };
 
 init();
 animate();
 
 function init() {
-  var container = document.getElementById("cube");
+  container = document.getElementById("cube");
   var width = container.clientWidth;
   var height = container.clientHeight;
 
@@ -26,12 +29,38 @@ function init() {
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(width, height);
   container.appendChild(renderer.domElement);
+
+  // dat.gui
+  var gui = new dat.GUI({ autoPlace: false });
+  gui.add(params, "rpm_x", -10, 10).step(1);
+  gui.add(params, "rpm_y", -10, 10).step(1);
+  gui.add(params, "rpm_z", -10, 10).step(1);
+  gui.close();
+  document.getElementById("param").appendChild(gui.domElement);
+
+  // add listener for window resize
+  window.addEventListener("resize", onWindowResize);
 }
 
 // animation
 function animate() {
+  var t = Date.now() * 0.001;
+  var dt = t - t_prev;
+  t_prev = t;
+
   requestAnimationFrame(animate);
-  mesh.rotation.x += 0.005;
-  mesh.rotation.y += 0.01;
+  mesh.rotation.x += (dt * params["rpm_x"] * Math.PI) / 30;
+  mesh.rotation.y += (dt * params["rpm_y"] * Math.PI) / 30;
+  mesh.rotation.z += (dt * params["rpm_z"] * Math.PI) / 30;
   renderer.render(scene, camera);
+}
+
+// reset camera and renderer on window resize
+function onWindowResize() {
+  var width = container.clientWidth;
+  var height = container.clientHeight;
+
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(width, height);
 }
